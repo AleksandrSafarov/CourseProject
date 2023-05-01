2e624c04165d196632728f4571bd03bd28be2541
sdvbjdkjv <riuihuirgfsdeuf.com>
2023-05-01 15:06:11 +0300

    register

--- chooser/main/views.py
+++ chooser/main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import *
+from django.contrib.auth.models import User
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        self.extra_context = {
+        }
+        return super().get_context_data(**kwargs)
+        
+    def get_success_url(self):
+        return reverse_lazy('index')
